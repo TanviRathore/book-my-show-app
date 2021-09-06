1bdb5b75d2f15ca8bf61ab59d272713515be8b90
Tanvi Rathore <tanvirathore1607@gmail.com>
2021-09-06 16:28:46 +0530

    Section Added

--- src/pages/Home.Page.jsx
+++ src/pages/Home.Page.jsx
@@ -123,48 +123,50 @@ function HomePage() {
         <>
             <HeroCarousel />
 
+           <div className="bg-darkBackground-50 container w-full">
             <div className="container mx-auto px-4 md:px-12 my-8">
-                <PosterSlider 
-                    title="Recommended Movies"
-                    subtitle="List of recommended Movies"
-                    posters={recommendedMovies}
-                    isDark={false}
-                />
-            </div>
-
-            <div className="container mx-auto px-4 md:px-12 my-8">
-                <h1 className="text-2xl font-bold text-gray-800 sm:ml-3 ml-0 my-3">
-                    The best of Entertainment
-                </h1>
-                <EntertainmentCardSlider />
-            </div>
-
-            <div className="bg-premier-800 py-12">
-                <div className="container mx-auto px-4 md:px-12 my-8">
-                    <div className="hidden md:flex">
-                        <img 
-                            src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
-                            alt="RuPay"
-                            className="w-full h-full"
-                        />
-                    </div>
                     <PosterSlider 
-                        title="Premiers"
-                        subtitle="Brand new releases every Friday"
-                        posters={premierMovies}
-                        isDark={true}
+                        title="Recommended Movies"
+                        subtitle="List of recommended Movies"
+                        posters={recommendedMovies}
+                        isDark={false}
                     />
                 </div>
-            </div>
 
-            <div className="container mx-auto px-4 md:px-12 my-8">
-                <PosterSlider 
-                    title="Online Streaming Events"
-                    subtitle=""
-                    posters={onlineStreamEvents}
-                    isDark={false}
-                />
-            </div>
+                <div className="container mx-auto px-4 md:px-12 my-8">
+                    <h1 className="text-2xl font-bold text-gray-800 sm:ml-3 ml-0 my-3">
+                        The best of Entertainment
+                    </h1>
+                    <EntertainmentCardSlider />
+                </div>
+
+                <div className="bg-premier-800 py-12">
+                    <div className="container mx-auto px-4 md:px-12 my-8">
+                        <div className="hidden md:flex">
+                            <img 
+                                src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+                                alt="RuPay"
+                                className="w-full h-full"
+                            />
+                        </div>
+                        <PosterSlider 
+                            title="Premiers"
+                            subtitle="Brand new releases every Friday"
+                            posters={premierMovies}
+                            isDark={true}
+                        />
+                    </div>
+                </div>
+
+                <div className="container mx-auto px-4 md:px-12 my-8">
+                    <PosterSlider 
+                        title="Online Streaming Events"
+                        subtitle=""
+                        posters={onlineStreamEvents}
+                        isDark={false}
+                    />
+                </div>
+           </div>
 
         </>
     );
